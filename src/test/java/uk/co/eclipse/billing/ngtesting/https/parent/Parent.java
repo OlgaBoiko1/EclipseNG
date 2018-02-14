2df6bff29696173b36e8516a7b1a81745ec06977
OlgaBoiko1 <olga.boiko.smart@gmail.com>
2018-02-14 17:28:39 +0000

    - Set up running tests on Selenium Grid - Set up running tests on FF

--- src/test/java/uk/co/eclipse/billing/ngtesting/https/parent/Parent.java
+++ src/test/java/uk/co/eclipse/billing/ngtesting/https/parent/Parent.java
@@ -6,17 +6,22 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.TestName;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import uk.co.eclipse.billing.ngtesting.https.libs.DataBaseData;
 import uk.co.eclipse.billing.ngtesting.https.libs.Properties;
 import uk.co.eclipse.billing.ngtesting.https.libs.Utils;
 import uk.co.eclipse.billing.ngtesting.https.pages.*;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.core.Is.is;
+import static uk.co.eclipse.billing.ngtesting.https.libs.Properties.browser;
 
 public class Parent {
     public WebDriver webDriver;
@@ -40,22 +45,33 @@ public class Parent {
     public TestName testName = new TestName();
 
     @Before
-    public void setUp() {
+    public void setUp() throws MalformedURLException {
         File file = new File("");
 
         if (Properties.getBrowser().equalsIgnoreCase("Chrome")){
             logger.info("Chrome will be started");
             File fileFF = new File("./drivers/chromedriver.exe");
             System.setProperty("webdriver.chrome.driver", fileFF.getAbsolutePath());
-            webDriver = new ChromeDriver();
+            //webDriver = new ChromeDriver();
+            webDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.chrome());
             logger.info("Chrome has been started successfully.");
+            webDriver.manage().window().maximize();
+        }
+        if (Properties.getBrowser().equalsIgnoreCase("FF")){
+            logger.info("FF will be started");
+            File fileFF = new File("./drivers/geckodriver.exe");
+            System.setProperty("webdriver.gecko.driver", fileFF.getAbsolutePath());
+            //webDriver = new FirefoxDriver();
+            webDriver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.firefox());
+            logger.info("FF has been started successfully.");
+            webDriver.manage().window().setSize(new Dimension(1920, 1080));
         }
         else logger.error("Browser can not be defined");
 
         pathToScreenshot = file.getAbsolutePath() + "\\target\\screenshots\\" + this.getClass().getPackage().getName()
                 + "\\" + this.getClass().getSimpleName() + "\\"
                 + this.testName.getMethodName() + "_" + Properties.getBrowser() + ".jpg";
-        webDriver.manage().window().maximize();
+        //webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         loginPage = new LoginPage(webDriver);
